eaf02b80e00c5f7c5c775d336713ea93253a4a87
Harshal <harshalbhave41@gmail.com>
2023-04-09 01:37:42 +0530

    Database Changes and Actor service

---
+++ MoviesTickets/Data/Services/IActorService.cs
@@ -0,0 +1,13 @@
+﻿using MoviesTickets.Models;
+
+namespace MoviesTickets.Data.Services
+{
+    public interface IActorService
+    {
+        Task<IEnumerable<Actor>> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
