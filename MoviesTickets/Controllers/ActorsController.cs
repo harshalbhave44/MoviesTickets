4986a0edc1395a2a86024fc73ea0ad56f17b6ad4
Harshal <harshalbhave41@gmail.com>
2023-04-09 00:18:58 +0530

    Controllers added

---
+++ MoviesTickets/Controllers/ActorsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly IActorsService _service;
+
+        public ActorsController(IActorsService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _service.GetAll();
+            return View(data);
+        }
+    }
+}
---
+++ MoviesTickets/Controllers/ProducersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoviesTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Producers.ToListAsync();
+            return View(allProducers);
+        }
+    }
+}
